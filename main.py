0ee0e239660a38c0f4f249a25fc9d8b428b4bd69
AlperYurtseven <yurtsevenalper@gmail.com>
2023-04-12 13:18:27 +0200

    data and column returner as numpy array added

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pandas as pd
+import numpy as np
 import argparse
+import sys
+import os
 
 # Compresses the binary table
 def compression_algorithm(input_file):
@@ -111,7 +114,7 @@ def decompress_file(compressed_file):
 
 
 # Prints decompressed file to given file name
-def decompress_file_printer(outfile_name, all_the_strains, outfile_type="tsv"):
+def decompress_file_printer(outfile_name, all_the_strains, outfile_type):
 
     seperator = None
 
@@ -135,9 +138,84 @@ def decompress_file_printer(outfile_name, all_the_strains, outfile_type="tsv"):
     return 0
 
 
+def cbt_to_array(compressed_file):
+
+    # Check if given file is properly formatted
+
+    if not compressed_file.endswiht(".cbt"):
+        print("Given file is not cbt format")
+        return -1
+
+    with open(compressed_file) as infile:
+        lines = infile.readlines()
+
+    default_value = str(lines[0].split(";")[0])
+
+    # Checks if it is binary matrix
+
+    if default_value not in ["0" ,"1"]:
+        print("Given table is not binary")
+        return -1
+    
+    all_the_strains = []
+
+    # Set the value which will printed to indexes
+    
+    if default_value == "0":
+        indexed_value = "1"
+    else:
+        indexed_value = "0"
+    
+    columns = lines[0][1:]
+
+    for line in lines[1:]:
+        splitted = line.split(";")
+        strain_name = splitted[0]
+        temp_list = [default_value for _ in range(len(columns.split(";"))+1)]
+        temp_list[0] = strain_name
+        for index in splitted[1:]:
+            temp_list[index] = indexed_value
+
+        all_the_strains.append(temp_list)
+
+    return_array = np.array(all_the_strains)
+
+    return return_array
+
+
+def cbt_columns(compressed_file):
+
+    # Check if given file is properly formatted
+
+    if not compressed_file.endswiht(".cbt"):
+        print("Given file is not cbt format")
+        return -1
+
+    with open(compressed_file) as infile:
+        lines = infile.readlines()
+
+    default_value = str(lines[0].split(";")[0])
+
+    # Checks if it is binary matrix
+
+    if default_value not in ["0" ,"1"]:
+        print("Given table is not binary")
+        return -1
+    
+    columns = lines[0][1:]
+
+    return_array = np.array(columns)
+
+    return return_array 
+
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description='Compression of Binary Mutation Tables')
+
+    parser.add_argument("-c", help="Compression flag", action="store_true")
+
+    parser.add_argument("-d", help="Decompression flag", action="store_true")
 
     parser.add_argument("-i", help="Path of input file", required=True)
 
@@ -145,16 +223,57 @@ if __name__ == "__main__":
 
     parser.add_argument("-t", help="Type of output file, either csv or tsv", default="tsv")
 
+    parser.add_argument("--override", help="If output file exist and want to override", action="store_true")
+
     args = parser.parse_args()
 
+    print(args)
+
+    if not os.path.exists(args.i):
+        print("Input file is not exist, please provide input file")
+        exit()
+
+    if os.path.exists(args.o):
+        if not args.override:
+            print("Output file is already exist, use --override if you want to override")
+            print("Warning : The old file will be overriden!")
+            exit()
+
+    if args.c and args.d:
+
+        print("You need to select either Compression -c or Decompression -d")
+        exit()
+
+    if args.c:
+
+        print("Compression starting")
+
+        # Maybe add randomized movie quotes here 
+
+        dictionary, columns, sel = compression_algorithm(args.i)
+        compressed_file_writer(args.o, dictionary, columns, sel)
+
+        print("Compression ended, compressed file can be found at %s" % args.o)
+
+        print("Have a nice day!")
+
+        exit()
+
+    if args.d:
+
+        print("Decompression starting")
+
+        decompress_file_printer(args.o, decompress_file(args.i), args.t)
+
+        print("Decompression ended, decompressed file can be found at %s" % args.o)
+
+        print("Have a nice day!")
+
+        exit()
+    
+    else:
+        print("You need to select either Compression -c or Decompression -d")
+        exit()
 
 
-# dictionary, columns, sel = compression_algorithm("./combined_binary_mutations_non_snp_corrected_0.05.tsv")
 
-# compressed_file_writer("./test.cbt", dictionary, columns, sel)
-
-# dictionary2, columns2, sel2 = compression_algorithm("/scratch/SCRATCH_SAS/alper/Mycobacterium/non_dropped/combined_binary_mutations.tsv")
-
-# compressed_file_writer("./test2.cbt", dictionary2, columns2, sel2)
-
-#print(arr)
